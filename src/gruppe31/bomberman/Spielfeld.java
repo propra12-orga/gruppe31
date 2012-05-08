7791a254ce010c9fc4079126e21c6ef4e14026ea
Meena Mathivanan <Meena.Mathivanan@uni-duesseldorf.de>
2012-05-08 23:33:10 +0200

    Brick Background added

--- src/gruppe31/bomberman/Spielfeld.java
+++ src/gruppe31/bomberman/Spielfeld.java
@@ -1,48 +1,59 @@
 package gruppe31.bomberman;
-import java.awt.Graphics ;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import javax.swing.*;
+
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.URL;
+import java.util.Random;
 
-public class Spielfeld extends JFrame{
-	JPanel pnlButton = new JPanel();
-    JButton begin = new JButton("START");
-	JButton stop = new JButton("STOP");
-	public Spielfeld(){
-		super("Bomberman");
-		setSize(300,300);
-		setLocation(300,300);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pnlButton.setBounds(800, 800, 200, 100);
-		begin.setBounds(300, 300, 20, 10);
-		stop.setBounds(300, 350, 20, 10);
-		pnlButton.add(begin);
-		pnlButton.add(stop);
-	    add(pnlButton);
-		setVisible(true);
-		pack();		
-	}
-	
-	public void paint(Graphics g) {
-		int i=0;
-		int j=0;
-        Graphics2D graphics2 = (Graphics2D) g;
-        Rectangle2D rectangle = new Rectangle2D.Float(300, 60, 600, 600);
-        graphics2.draw(rectangle);
-        for(i=300;i<=900; i=i+60){
-        	for(j= 60;j<= 660;j=j+60){
-        	      graphics2.drawLine(i, j, 900, j);
-        	      graphics2.drawLine(i,j, i,660); 
-        	} 	
-        }
-    }
-	
-     public static void main(String[] args){
-		 Spielfeld spielfeld = new Spielfeld();
-		
-	}
+import javax.annotation.Resource;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Spielfeld {
+
+	public static void main(String[] args) {
+		JFrame frame= new JFrame("BorderLayout Frame");
+		JPanel pa= new JPanel();
+		JPanel pa2= new JPanel();
+		JPanel pa3 = new JPanel();
+		JButton begin = new JButton("START");
+		JButton stop = new JButton("STOP");
+		//ImageIcon icon1 = new ImageIcon("bricks1.jpg");
+		ImageIcon icon2 = new ImageIcon("/home/meena/bricks2.jpg");	
+		frame.add(pa);
+		pa.setLayout(new BorderLayout());
+		pa3.setLayout(new FlowLayout());
+		pa3.add(new JButton("START"));
+		pa3.add(new JButton("STOP"));
+		pa.add(pa3,BorderLayout.NORTH);
+		pa.add(new JButton(""), BorderLayout.EAST);
+		pa.add(new JButton(""), BorderLayout.WEST);
+		pa.add(new JButton(""),BorderLayout.SOUTH);
+		pa2.setLayout(new GridLayout(15, 15));
+
+	    for(int y = 0; y < 15; y++) {
+	      for(int x = 0; x < 15; x++) {
+	       JLabel l = new JLabel();
+	       Random random = new Random();
+	       l.setIcon(icon2);
+	        pa2.add(l);
+	      }
+	    }
+	    pa.add(pa2,BorderLayout.CENTER);
+		frame.setSize(300,300);
+		frame.setVisible(true);
+		frame.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		});
+	}  
 }
-
 
 
