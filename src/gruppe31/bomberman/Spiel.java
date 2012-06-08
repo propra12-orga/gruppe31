0d8267d1793c121270f60fa0689bd9c2cecdd4eb
Meena Mathivanan <Meena.Mathivanan@uni-duesseldorf.de>
2012-06-08 20:04:47 +0200

    unzerstörbare Wand eingefügt

--- src/gruppe31/bomberman/Spiel.java
+++ src/gruppe31/bomberman/Spiel.java
@@ -1,6 +1,5 @@
 package gruppe31.bomberman;
 
-
 import java.awt.BorderLayout;
 import java.awt.Frame;
 import java.awt.GridLayout;
@@ -23,7 +22,8 @@ import javax.swing.Timer;
 
 public class Spiel extends JFrame implements KeyListener, ActionListener {
 
-	public boolean[][] isBrickCell = new boolean[20][20]; 
+	public boolean[][] isBreakableBrickCell = new boolean[20][20];
+	public boolean[][] isUnbreakableBrickCell = new boolean[20][20];
 	public JLabel[][] cell = new JLabel[20][20];
 	int playerPositionX = 0;
 	int playerPositionY = 0;
@@ -31,31 +31,27 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 	int bombPositionY = 0;
 	int exitPositionX = 0;
 	int exitPositionY = 0;
-
-	ImageIcon playerIcon = new ImageIcon(this.getClass().getResource("mario_small_1.jpg"));
+	
+    // Bilder
+	ImageIcon playerIcon = new ImageIcon(this.getClass().getResource("player.jpg"));
 	ImageIcon bombIcon = new ImageIcon(this.getClass().getResource("bomb.jpg"));
-	ImageIcon playerAndBombIcon = new ImageIcon(this.getClass().getResource("mario_with_bomb1.jpg"));
+	ImageIcon playerAndBombIcon = new ImageIcon(this.getClass().getResource("playerAndBomb.jpg"));
 	ImageIcon exitIcon = new ImageIcon(this.getClass().getResource("exit.jpeg"));
+	ImageIcon breakableBrickIcon = new ImageIcon(this.getClass().getResource("breakableWall.jpg"));
+	ImageIcon unbreakableBrickIcon = new ImageIcon(this.getClass().getResource("unbreakableWall.jpg"));
 	boolean isBombInPlayground = false;
 	Timer bombTimer;
 
 	Spiel spiel = null;
 
-
 	public Spiel(){
-
 	}
 
-
 	public void initaliseSpiel() {
-		//JFrame frame= new JFrame("Bomberman");
-		//frame.addKeyListener(this);
 		createMenu();
 		addKeyListener(this);
 		JPanel pa= new JPanel();
 		JPanel pa2= new JPanel();
-		ImageIcon icon2 = new ImageIcon(this.getClass().getResource("bricks2.jpg"));	
-		//frame.add(pa);
 		this.add(pa);
 		pa.setLayout(new BorderLayout());
 		pa.add(new JButton(""),BorderLayout.NORTH);
@@ -63,6 +59,8 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 		pa.add(new JButton(""), BorderLayout.WEST);
 		pa.add(new JButton(""),BorderLayout.SOUTH);
 		pa2.setLayout(new GridLayout(20, 20));
+		
+		// zwei Zufallzahlen werden generiert , die zerstörbare und die unzerstörbare Wände werden eingefügt
 
 		for(int i = 0; i < 20; i++) {
 			for(int j = 0; j < 20; j++) {
@@ -72,53 +70,50 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 				int random1 = generator.nextInt(20);
 				int random2 = generator.nextInt(20);
 				if ((j == random1) || (i == random1) || (j==random1+1) ||(i == random1+1)){
-					l.setIcon(icon2);
-					isBrickCell[i][j] = true;
+					l.setIcon(breakableBrickIcon);
+					isBreakableBrickCell[i][j] = true;
 				}
 				else if ((j ==random2) || (i == random2)||(j ==random2 +1)||(i==random2 +1)){
-					l.setIcon(icon2);
-					isBrickCell[i][j] = true;
+					l.setIcon(unbreakableBrickIcon);
+					isUnbreakableBrickCell[i][j] = true;
 				}
 				pa2.add(l);
 
 			}
 		}
+		// In mittlere Teil von BorderLayout , wird die 20x20 GridLayout eingefügt
 		pa.add(pa2,BorderLayout.CENTER);
-		//	frame.setSize(300,300);
-		//	frame.setVisible(true);
-		//	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//this.setSize(300,300);
 		this.setExtendedState(Frame.MAXIMIZED_BOTH);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		//Keep (0,0) (0,1) (1,0) (1,1) (2,0) (2,1) free of bricks 
+		// (0,0) (0,1) (1,0) (1,1) (2,0) (2,1) mit Absicht freigelassen 
 		cell[0][0].setIcon(null);
 		cell[0][1].setIcon(null);
 		cell[1][0].setIcon(null);
 		cell[1][1].setIcon(null);
 		cell[2][0].setIcon(null);
 		cell[2][1].setIcon(null);
-		isBrickCell[0][0] = false;
-		isBrickCell[0][1] = false;
-		isBrickCell[1][0] = false;
-		isBrickCell[1][1] = false;
-		isBrickCell[2][0] = false;
-		isBrickCell[2][1] = false;
+		isBreakableBrickCell[0][0] = false;
+		isBreakableBrickCell[0][1] = false;
+		isBreakableBrickCell[1][0] = false;
+		isBreakableBrickCell[1][1] = false;
+		isBreakableBrickCell[2][0] = false;
+		isBreakableBrickCell[2][1] = false;
 
-		//Position the player in 0,0
+		//Position der Spieler in 0,0 zu beginnen
 		cell[0][0].setIcon(playerIcon);
 		placeExitDoor();
 		this.setFocusable(true);
 	}
 	public void createMenu() {
-		// Create the menu bar
+		// menuBar wird  erzeugt
 		JMenuBar menuBar = new JMenuBar();
-		// Create a menu
+		// menu wird wird erzeugt und in menuBar eingefügt
 		JMenu menu = new JMenu("Bomberman");
 		menuBar.add(menu);
 
-		// Create a menu item
+		// 2 menuitem wird erzeugt mit ActionListener in menu eingefügt  
 		JMenuItem restart = new JMenuItem("Restart");
 		restart.addActionListener(this);
 		menu.add(restart);
@@ -127,11 +122,13 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 		exit.addActionListener(this);
 		menu.add(exit);
 
-		// Install the menu bar in the frame
+		// menubar in frame eingesetzt
 		this.setJMenuBar(menuBar);
 	}
 	public void placeExitDoor() {
-		//Place the Exit Door somewhere between 15th to 19th row and 15th to 19th column.
+		// Eine Zufallnummer generiert und die Ausgangtür innerhalb 15.- 19. -reihe und -Spalten unter 
+		// eine zerstörbare Wand versteckt
+		
 		Random generator = new Random();
 		int row = 0;
 		int column = 0;
@@ -145,24 +142,25 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 		}
 		exitPositionX = row;
 		exitPositionY = column;
-		cell[row][column].setIcon(exitIcon);
-		isBrickCell[row][column] = false;
+		cell[row][column].setIcon(breakableBrickIcon);
+		isBreakableBrickCell[row][column] = true;
 	}
-
+    // startGame methode wird definiert für das Spiel anzufangen
 	public void startGame() {
 		this.initaliseSpiel();
 	}
 
+	
 	public static void main(String[] args) {
 		Spiel x = new Spiel();
+		// Spiel ist mit startGame methode gestartet
 		x.startGame();
 	}
 	public void exitDoorAction() {
 		System.out.println("ExitDoor X: " + exitPositionX + " ExitDoor Y: " + exitPositionY);
-		//if the curren player position equals Exit Door Position, Restart game.
+		//Falls die Spieler position gleich wie die Ausgangstür Position, wird das Spiel gestartet 
 		if (playerPositionX == exitPositionX && playerPositionY == exitPositionY) {
 			System.out.println("Player exits via door.");
-			//JDialog dialog = new JDialog(this, true);
 			JOptionPane.showMessageDialog(this,"Game Over. Restarting...");
 			this.setVisible(false);
 			spiel = new Spiel();
@@ -176,7 +174,7 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 		if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_KP_LEFT) {
 			System.out.println("left key");
 			System.out.println("Current Player Position: " + playerPositionX + "," + playerPositionY);
-			if (playerPositionY >= 1 && !isBrickCell[playerPositionX][playerPositionY - 1]) {
+			if (playerPositionY >= 1 && isCellFreeFromBricks(playerPositionX, playerPositionY - 1)) {
 				if (playerAndBombIcon.equals(cell[playerPositionX][playerPositionY].getIcon())) {
 					cell[playerPositionX][playerPositionY].setIcon(bombIcon);
 				} else {
@@ -193,7 +191,7 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 		} else if ((keyCode == KeyEvent.VK_RIGHT || keyCode == KeyEvent.VK_KP_RIGHT)) {
 			System.out.println("right key");
 			System.out.println("Current Player Position: " + playerPositionX + "," + playerPositionY);
-			if (playerPositionY <= 18 && !isBrickCell[playerPositionX][playerPositionY + 1]) {
+			if (playerPositionY <= 18 && isCellFreeFromBricks(playerPositionX, playerPositionY + 1)) {
 				if (playerAndBombIcon.equals(cell[playerPositionX][playerPositionY].getIcon())) {
 					cell[playerPositionX][playerPositionY].setIcon(bombIcon);
 				} else {
@@ -211,7 +209,7 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 		}else if ((keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_KP_UP)) {
 			System.out.println("up key");
 			System.out.println("Current Player Position: " + playerPositionX + "," + playerPositionY);
-			if (playerPositionX >= 1 && !isBrickCell[playerPositionX - 1][playerPositionY]) {
+			if (playerPositionX >= 1 && isCellFreeFromBricks(playerPositionX - 1, playerPositionY)) {
 				if (playerAndBombIcon.equals(cell[playerPositionX][playerPositionY].getIcon())) {
 					cell[playerPositionX][playerPositionY].setIcon(bombIcon);
 				} else {
@@ -229,7 +227,7 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 		}else if ((keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_KP_DOWN)) {
 			System.out.println("down");
 			System.out.println("Current Player Position: " + playerPositionX + "," + playerPositionY);
-			if (playerPositionX <= 18 && !isBrickCell[playerPositionX + 1][playerPositionY]) {
+			if (playerPositionX <= 18 && isCellFreeFromBricks(playerPositionX + 1, playerPositionY)) {
 				if (playerAndBombIcon.equals(cell[playerPositionX][playerPositionY].getIcon())) {
 					cell[playerPositionX][playerPositionY].setIcon(bombIcon);
 				} else {
@@ -260,8 +258,8 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 		System.out.println(e.getActionCommand());
 		if (e.getActionCommand() != null && e.getActionCommand().equalsIgnoreCase("Restart")) {
 			this.setVisible(false);
-			spielfeld = new Spielfeld();
-			spielfeld.initaliseSpielfeld();
+			spiel = new Spiel();
+			spiel.initaliseSpiel();
 		} else if (e.getActionCommand() != null && e.getActionCommand().equalsIgnoreCase("Exit")) {
 			System.exit(0);
 		} else {
@@ -274,71 +272,90 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 		}
 	}
 
-	public void isPlayerOnBomb(int x, int y) {
-		if (playerIcon.equals(cell[x][y].getIcon()) || playerAndBombIcon.equals(cell[x][y].getIcon())) {
-			JOptionPane.showMessageDialog(this,"Player Died. Game Over. Restarting...");
-			this.setVisible(false);
-			spielfeld = new Spielfeld();
-			spielfeld.initaliseSpielfeld();
-		}
-		
-	}
+	
 	public void explodeBomb() {
-		//(x-1,y-1),(x-1, y),(x-1, y+1) 
-		//(x, y-1),(x,y),(x, y+1)
-		//(x+1, y-1), (x+1, y),(x+1,y+1)
-
 		//Reihe x-1
 		if (bombPositionX - 1 >= 0) {
 			if (bombPositionY - 1 >= 0) {
-				isPlayerOnBomb(bombPositionX -1, bombPositionY -1);
-				cell[bombPositionX -1][bombPositionY -1].setIcon(null);
-				isBrickCell[bombPositionX -1][bombPositionY -1] = false;
+				clearCellIfBreakable(bombPositionX - 1, bombPositionY -1);
 			}
-			isPlayerOnBomb(bombPositionX -1, bombPositionY);
-			cell[bombPositionX -1][bombPositionY].setIcon(null);
-			isBrickCell[bombPositionX -1][bombPositionY] = false;
+			clearCellIfBreakable(bombPositionX - 1, bombPositionY);
 			if (bombPositionY + 1 <= 19) {
-				isPlayerOnBomb(bombPositionX -1, bombPositionY + 1);
-				cell[bombPositionX -1][bombPositionY + 1].setIcon(null);
-				isBrickCell[bombPositionX -1][bombPositionY + 1] = false;
+				clearCellIfBreakable(bombPositionX - 1, bombPositionY + 1);
 			}
 		}
 
 		//Reihe x
 		if (bombPositionY - 1 >= 0) {
-			isPlayerOnBomb(bombPositionX, bombPositionY -1);
-			cell[bombPositionX][bombPositionY -1].setIcon(null);
-			isBrickCell[bombPositionX][bombPositionY -1] = false;
+			clearCellIfBreakable(bombPositionX, bombPositionY - 1);
 		}
-		isPlayerOnBomb(bombPositionX, bombPositionY);
-		cell[bombPositionX][bombPositionY].setIcon(null);
-		isBrickCell[bombPositionX][bombPositionY] = false;
+		clearCellIfBreakable(bombPositionX, bombPositionY);
 		if (bombPositionY + 1 <= 19) {
-			isPlayerOnBomb(bombPositionX, bombPositionY + 1);
-			cell[bombPositionX][bombPositionY + 1].setIcon(null);
-			isBrickCell[bombPositionX][bombPositionY + 1] = false;
+			clearCellIfBreakable(bombPositionX, bombPositionY + 1);
 		}
 
 		//Reihe x-1
 		if (bombPositionX + 1 <= 19) {
 			if (bombPositionY - 1 >= 0) {
-				isPlayerOnBomb(bombPositionX + 1, bombPositionY - 1);
-				cell[bombPositionX + 1][bombPositionY -1].setIcon(null);
-				isBrickCell[bombPositionX + 1][bombPositionY - 1] = false;
+				clearCellIfBreakable(bombPositionX + 1, bombPositionY - 1);
 			}
-			isPlayerOnBomb(bombPositionX + 1, bombPositionY);
-			cell[bombPositionX + 1][bombPositionY].setIcon(null);
-			isBrickCell[bombPositionX + 1][bombPositionY] = false;
+			clearCellIfBreakable(bombPositionX + 1, bombPositionY);
 			if (bombPositionY + 1 <= 19) {
-				isPlayerOnBomb(bombPositionX + 1, bombPositionY + 1);
-				isBrickCell[bombPositionX + 1][bombPositionY + 1] = false;
-				cell[bombPositionX + 1][bombPositionY + 1].setIcon(null);
+				clearCellIfBreakable(bombPositionX + 1, bombPositionY + 1);
 			}
 		}
 	}
 
+	/**
+	 * clear the cell x,y of bricks (if it is breakable brick).
+	 * @param x
+	 * @param y
+	 */
+	public void clearCellIfBreakable(int x, int y) {
+		restartIfPlayerOnBombField(x, y);
+		//wenn es ein Ausgangstür position ist
+		if (x == exitPositionX && y == exitPositionY) {
+			if (exitIcon.equals(cell[x][x].getIcon())) {
+				return;
+			} else {
+				cell[x][y].setIcon(exitIcon);
+				isBreakableBrickCell[x][y] = false;
+				return;
+			}
+		}
+		//die Zell entleeren,nur wann die ein zerstörbares Wand hat
+		if (!isUnbreakableBrickCell[x][y]) {
+			cell[x][y].setIcon(null);
+			isBreakableBrickCell[x][y] = false;
+		}
+	}
+	/**
+	 * Restart the game if player is in the Cell x, y when the bomb explodes.
+	 * @param x
+	 * @param y
+	 */
+	public void restartIfPlayerOnBombField(int x, int y) {
+		if (playerIcon.equals(cell[x][y].getIcon()) || playerAndBombIcon.equals(cell[x][y].getIcon())) {
+			JOptionPane.showMessageDialog(this,"Player Dead. Game Over. Restarting...");
+			this.setVisible(false);
+			spiel = new Spiel();
+			spiel.initaliseSpiel();
+		}
+	}
 
+	/**
+	 * Check if a cell is free of bricks
+	 * @param x
+	 * @param y
+	 * @return true, if the cell is free from Brick,
+	 *         false, otherwise
+	 */
+	public boolean isCellFreeFromBricks(int x, int y) {
+		if (!isBreakableBrickCell[x][y] && !isUnbreakableBrickCell[x][y]) {
+			return true;
+		}
+		return false;
+	}
 
 	public void keyReleased(KeyEvent e) {
 	}
@@ -346,6 +363,4 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 	public void keyTyped(KeyEvent arg0) {
 	}
 }
-
-
 
