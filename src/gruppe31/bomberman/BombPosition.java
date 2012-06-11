34b3081fc9c26b9953d9b4472e1a41d143fe8fd1
ilham <ilham@ilham-AOD255E.(none)>
2012-06-11 13:18:08 +0100

    Bombposition klasse

---
+++ src/gruppe31/bomberman/BombPosition.java
@@ -0,0 +1,22 @@
+package gruppe31.bomberman;
+
+public class BombPosition {
+
+	private int positionX;
+	private int positionY;
+	
+	
+	public int getPositionX() {
+		return positionX;
+	}
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+	public int getPositionY() {
+		return positionY;
+	}
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+}
+
