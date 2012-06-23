1f004fd7f20a4becdb9dea74fedd6026da9b5b4f
ilham <ilham@ilham-AOD255E.(none)>
2012-06-23 22:43:06 +0100

    spiel

--- src/gruppe31/bomberman/Spiel.java
+++ src/gruppe31/bomberman/Spiel.java
@@ -1,5 +1,7 @@
 package gruppe31.bomberman;
 
+
+
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Frame;
@@ -440,8 +442,8 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 	 * Diese Methode überprüft, ob der Icon ,playerIcon / playerAndBombIcon ist und gibt die Nachricht 
 	 * "Player Dead.Game Over.Restarting..." aus. Eine neue Objekt wird erzeugt und die Methode initialiseSpiel()
 	 * wird aufgerufen, damit ein neues Spiel beginnt.
-	 * @param x Reihe der JLabelarray
-	 * @param y Spalte der JLabelarray
+	 * @param x:Reihe der JLabelarray
+	 * @param y:Spalte der JLabelarray
 	 */
 	public void restartIfPlayerOnBombField(int x, int y) {
 		// informiert dass der Speiler tot ist und wird das Spiel neue gestartet.
@@ -456,9 +458,10 @@ public class Spiel extends JFrame implements KeyListener, ActionListener {
 	/**
 	 * Diese Methode überprüft, ob der boolean isBreakableBrickCell und isUnbreakableBrickCell 
 	 * in x,y false ist 
-	 * @param x Reihe der Booleanarray
-	 * @param y Spalten der Booleanarray
-	 * @return true wenn
+	 * @param x: Reihe der Booleanarray
+	 * @param y: Spalten der Booleanarray
+	 * @return true: wenn beide boolean false sind
+	 * @return false: wenn beide boolean nicht false sind.
 	 */
 	public boolean isCellFreeFromBricks(int x, int y) {
 		//überprüft ob der Zell frei ist und gibt true züruck, sonst wird false züruckgegeben.
